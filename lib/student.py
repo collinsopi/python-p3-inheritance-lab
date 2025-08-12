bf5f14c71527b41c1750e68c8bba0f45fa86de25
Collins Opiayo <opiayocollins2@gmail.com>
2025-08-12 15:20:02 +0300

    inheritance

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,12 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        # Call the parent class's __init__ method
+        super().__init__(first_name, last_name)
+        # Initialize the knowledge attribute as an empty list
+        self.knowledge = []
+
+    def learn(self, new_knowledge):
+        # Add a new string to the knowledge list
+        self.knowledge.append(new_knowledge)
